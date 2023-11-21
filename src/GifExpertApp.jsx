7121dc2ddaf945b0f344d89227b574539d9bf803
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-11-21 09:59:14 -0300

    Creacion de proyecto

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball'])
+    
+    return (
+        <>
+            {/* titulo */}
+            <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Listado de Gif */}
+            <ol>
+                {categories.map(category => {
+                    return <li key={category}>{category}</li>
+                })}
+                
+            </ol>
+                {/* Gif Item */}
+        </>
+    )
+}
