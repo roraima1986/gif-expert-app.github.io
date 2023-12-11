3a49bd24fb732c90154ce17784dbded8d2a8b949
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-12-11 15:17:29 -0300

    docs

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
-import { GifExpertApp } from './assets/GifExpertApp'
+import { GifExpertApp } from './GifExpertApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
